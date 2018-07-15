d7497ffb363f474fcc2f7df7ff09031a8d610835
lenamoiseeva <40036816+lenamoiseeva@users.noreply.github.com>
2018-07-15 19:26:06 +0300

    Update Collections.java

--- src/Collections.java
+++ src/Collections.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
-import java.util.ArrayList;
 
 public class Collections {
     public static void main(String[] args) throws FileNotFoundException {
